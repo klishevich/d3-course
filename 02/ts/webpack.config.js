05f2181e715fae3bf869bc7c1b5068dcacf81787
klishevich <busation@gmail.com>
2024-03-16 16:55:30 +0900

    create an empty TypeScript Webpack project

---
+++ 02/ts/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+});
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    htmlPlugin
+  ]
+};
